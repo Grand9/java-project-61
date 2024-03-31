c822e8bbbb8b3d0bb3b9519a5178033b3d184d3b
Arlan <Arlan110022@Gmail.com>
2024-03-31 15:08:08 +0500

    added GCD find game

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -60,27 +60,57 @@ public class Games {
                 userAnswer = scanner.nextInt();
             } else {
                 System.out.println("Invalid input. Please enter a numeric answer.");
-                scanner.next(); // Clear the invalid input
+                scanner.next(); // Чистка неверного ввода
                 continue; // Пропускаем итерацию цикла и продолжаем игру
             }
 
-            int correctAnswer;
-            switch (sign) {
-                case '+':
-                    correctAnswer = randomNumber1 + randomNumber2;
-                    break;
-                case '-':
-                    correctAnswer = randomNumber1 - randomNumber2;
-                    break;
-                case '*':
-                    correctAnswer = randomNumber1 * randomNumber2;
-                    break;
-                default:
-                    correctAnswer = 0; // Default value
-                    break;
-            }
+            int correctAnswer = switch (sign) {
+                case '+' -> randomNumber1 + randomNumber2;
+                case '-' -> randomNumber1 - randomNumber2;
+                case '*' -> randomNumber1 * randomNumber2;
+                default -> 0; // Default value
+            };
 
             if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+                correctAnswersCount++;
+            } else {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '"
+                        + correctAnswer + "'.");
+                System.out.println("Let's try again, " + userName + "!");
+                correctAnswersCount = 0; // Сброс счетчика правильных ответов
+            }
+        }
+
+        System.out.println("Congratulations, " + userName + "!");
+    }
+    public static void gcd(Scanner scanner) {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        Random random = new Random();
+        int correctAnswersCount = 0;
+
+        while (correctAnswersCount < 3) {
+            int randomNumber1 = random.nextInt(50);
+            int randomNumber2 = random.nextInt(50);
+            System.out.println("Question: " + randomNumber1 + " " + randomNumber2);
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.next();
+
+            int correctAnswer;
+            // Находим наибольший общий делитель
+            while (randomNumber2 != 0) {
+                int temp = randomNumber2;
+                randomNumber2 = randomNumber1 % randomNumber2;
+                randomNumber1 = temp;
+            }
+            correctAnswer = randomNumber1;
+
+            if (userAnswer.equals(String.valueOf(correctAnswer))) {
                 System.out.println("Correct!");
                 correctAnswersCount++;
             } else {
