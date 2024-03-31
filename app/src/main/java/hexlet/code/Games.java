b8605f5b3fbc3f4eaa8e1bf4254c524551a5aa34
Arlan <Arlan110022@Gmail.com>
2024-03-31 14:33:34 +0500

    added calculator game

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -34,4 +34,61 @@ public class Games {
 
         System.out.println("Congratulations, " + userName + "!");
     }
+
+    public static void calculator(Scanner scanner) {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("What is the result of the expression?");
+
+        Random random = new Random();
+        int correctAnswersCount = 0;
+
+        while (correctAnswersCount < 3) {
+            int randomNumber1 = random.nextInt(100);
+            int randomNumber2 = random.nextInt(100);
+            int signIndex = random.nextInt(3);
+            char[] signs = {'+', '-', '*'};
+            char sign = signs[signIndex];
+
+            System.out.println("Question: " + randomNumber1 + " " + sign + " " + randomNumber2);
+            System.out.print("Your answer: ");
+            int userAnswer;
+            if (scanner.hasNextInt()) {
+                userAnswer = scanner.nextInt();
+            } else {
+                System.out.println("Invalid input. Please enter a numeric answer.");
+                scanner.next(); // Clear the invalid input
+                continue; // Пропускаем итерацию цикла и продолжаем игру
+            }
+
+            int correctAnswer;
+            switch (sign) {
+                case '+':
+                    correctAnswer = randomNumber1 + randomNumber2;
+                    break;
+                case '-':
+                    correctAnswer = randomNumber1 - randomNumber2;
+                    break;
+                case '*':
+                    correctAnswer = randomNumber1 * randomNumber2;
+                    break;
+                default:
+                    correctAnswer = 0; // Default value
+                    break;
+            }
+
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+                correctAnswersCount++;
+            } else {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                System.out.println("Let's try again, " + userName + "!");
+                correctAnswersCount = 0; // Сброс счетчика правильных ответов
+            }
+        }
+
+        System.out.println("Congratulations, " + userName + "!");
+    }
 }
