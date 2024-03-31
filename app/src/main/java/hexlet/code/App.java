d59e4f8672f2012a4da9f8bfd6e1635eaa3600ac
Arlan <Arlan110022@Gmail.com>
2024-03-31 13:40:07 +0500

    added new case in Cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,33 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.welcomeUser();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                Cli.welcomeUser();
+                break;
+            case 0:
+                System.out.println("Goodbye!");
+                break;
+            case 2:
+                Cli.startEvenGame(scanner);
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a valid game number.");
+                break;
+        }
+
+        scanner.close();
     }
 }
