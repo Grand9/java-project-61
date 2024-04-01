04047f71c3e7a5ac4bea3a836fcb8fec1e72b3d5
Arlan <Arlan110022@Gmail.com>
2024-04-01 19:40:16 +0500

    magic numbers change

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,40 +3,48 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class App {
+    private static final int GREET_GAME = 1;
+    private static final int EVEN_GAME = 2;
+    private static final int CALC_GAME = 3;
+    private static final int GCD_GAME = 4;
+    private static final int PROGRESSION_GAME = 5;
+    private static final int PRIME_GAME = 6;
+    private static final int EXIT_GAME = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
+        System.out.println(GREET_GAME + " - Greet");
+        System.out.println(EVEN_GAME + " - Even");
+        System.out.println(CALC_GAME + " - Calc");
+        System.out.println(GCD_GAME + " - GCD");
+        System.out.println(PROGRESSION_GAME + " - Progression");
+        System.out.println(PRIME_GAME + " - Prime");
+        System.out.println(EXIT_GAME + " - Exit");
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
 
         switch (choice) {
-            case 1:
+            case GREET_GAME:
                 Cli.welcomeUser();
                 break;
-            case 0:
+            case EXIT_GAME:
                 System.out.println("Goodbye!");
                 break;
-            case 2:
+            case EVEN_GAME:
                 Games.startEvenGame(scanner);
                 break;
-            case 3:
+            case CALC_GAME:
                 Games.calculator(scanner);
                 break;
-            case 4:
+            case GCD_GAME:
                 Games.gcd(scanner);
                 break;
-            case 5:
+            case PROGRESSION_GAME:
                 Games.progression(scanner);
                 break;
-            case 6:
+            case PRIME_GAME:
                 Games.prime(scanner);
                 break;
             default:
