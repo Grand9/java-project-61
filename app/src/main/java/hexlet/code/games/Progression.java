a757d93c66af3d1938f5752c9159a64ab0fc4e33
Arlan <Arlan110022@Gmail.com>
2024-04-22 13:23:56 +0500

    for changes

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -3,8 +3,6 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
-import java.util.Arrays;
-
 public class Progression {
 
     private static final int PROGRESSION_LENGTH = 10;
@@ -15,14 +13,16 @@ public class Progression {
         for (int i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
             int start = Utils.generateNumber(0, Utils.RANDOM_UPPER_BOUND);
             int step = Utils.generateNumber(1, PROGRESSION_LENGTH);
-            int[] progression = generateArithmeticProgression(start, step);
             int missIndex = Utils.generateNumber(0, PROGRESSION_LENGTH - 1);
 
-            progression[missIndex] = -1;
-            String question = formatProgressionQuestion(progression, missIndex);
+            String[] progression = generateArithmeticProgression(start, step);
+            progression[missIndex] = "..";
+
+            String question = String.join(" ", progression);
+            String answer = String.valueOf(start + step * missIndex);
 
             gameData[i][0] = question;
-            gameData[i][1] = String.valueOf(start + step * missIndex);
+            gameData[i][1] = answer;
         }
 
         return gameData;
@@ -34,19 +34,12 @@ public class Progression {
         Engine.startGame(gameData, gameDescription);
     }
 
-    private static int[] generateArithmeticProgression(int first, int step) {
-        int[] progression = new int[Progression.PROGRESSION_LENGTH];
+    private static String[] generateArithmeticProgression(int first, int step) {
+        String[] progression = new String[Progression.PROGRESSION_LENGTH];
         for (int i = 0; i < Progression.PROGRESSION_LENGTH; i++) {
-            progression[i] = first + i * step;
+            progression[i] = String.valueOf(first + i * step);
         }
         return progression;
     }
 
-    private static String formatProgressionQuestion(int[] progression, int missIndex) {
-        progression[missIndex] = 0;
-        return String.join(" ",
-                Arrays.stream(progression)
-                        .mapToObj(num -> num == 0 ? ".." : String.valueOf(num))
-                        .toArray(String[]::new));
-    }
 }
